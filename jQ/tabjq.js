6c0df511bb1db61cff963117ad895a37c022e6e5
franklinvjustin <fdas161@gmail.com>
2022-02-01 18:20:36 +0530

    New Commit

---
+++ jQ/tabjq.js
@@ -0,0 +1,8 @@
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById(cityName).style.display = "block";
+}
